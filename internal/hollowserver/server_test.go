75ddfe26b143864076d70b654e2506c70b168976
Nicole Renée Hubbard <code@nicole.dev>
2021-07-14 17:15:21 -0500

    Don't run tests that use the database in parallel, it breaks

--- internal/hollowserver/server_test.go
+++ internal/hollowserver/server_test.go
@@ -13,6 +13,19 @@ import (
 	"go.metalkube.net/hollow/internal/hollowserver"
 )
 
+func TestUnknownRoute(t *testing.T) {
+	hs := hollowserver.Server{Logger: zap.NewNop()}
+	s := hs.NewServer()
+	router := s.Handler
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequestWithContext(context.TODO(), "GET", "/a/route/that/doesnt/exist", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 404, w.Code)
+	assert.Equal(t, `{"message":"invalid request - route not found"}`, w.Body.String())
+}
+
 func TestHealthzRoute(t *testing.T) {
 	hs := hollowserver.Server{Logger: zap.NewNop()}
 	s := hs.NewServer()
