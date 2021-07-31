a4bb3b3734daed1d3af5f26f4708023345de81e9
Nicole Renée Hubbard <code@nicole.dev>
2021-07-30 22:56:57 -0500

    Add db attributes test

--- internal/db/attributes_test.go
+++ internal/db/attributes_test.go
@@ -1,8 +1,10 @@
 package db_test
 
 import (
+	"encoding/json"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"gorm.io/datatypes"
 
@@ -31,5 +33,101 @@ func TestCreateAttributes(t *testing.T) {
 		} else {
 			assert.NoError(t, err, tt.testName)
 		}
+	}
+}
+
+func TestDeleteAttributes(t *testing.T) {
+	s := db.DatabaseTest(t)
+
+	err := s.DeleteAttributes(&db.FixtureAttributesDoryMetadata)
+	assert.NoError(t, err)
+}
+
+func TestGetAttributesByServerUUID(t *testing.T) {
+	s := db.DatabaseTest(t)
+
+	var testCases = []struct {
+		testName    string
+		u           uuid.UUID
+		expectUUIDs []uuid.UUID
+	}{
+		{"happy path", db.FixtureServerDory.ID, []uuid.UUID{db.FixtureAttributesDoryMetadata.ID, db.FixtureAttributesDoryOtherdata.ID}},
+		{"not found server uuid", uuid.New(), []uuid.UUID{}},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.testName, func(t *testing.T) {
+			attrs, count, err := s.GetAttributesByServerUUID(tt.u, nil)
+
+			assert.NoError(t, err)
+			assert.Len(t, attrs, int(count))
+
+			var respIDs []uuid.UUID
+			for _, a := range attrs {
+				respIDs = append(respIDs, a.ID)
+			}
+
+			assert.ElementsMatch(t, tt.expectUUIDs, respIDs)
+		})
+	}
+}
+
+func TestGetAttributesByServerUUIDAndNamespace(t *testing.T) {
+	s := db.DatabaseTest(t)
+
+	var testCases = []struct {
+		testName         string
+		u                uuid.UUID
+		ns               string
+		expectedID       uuid.UUID
+		expectedNotFound bool
+	}{
+		{"happy path", db.FixtureServerDory.ID, db.FixtureNamespaceMetadata, db.FixtureAttributesDoryMetadata.ID, false},
+		{"not found server uuid", uuid.New(), db.FixtureNamespaceMetadata, uuid.Nil, true},
+		{"not found namespace", db.FixtureServerDory.ID, "unknown", uuid.Nil, true},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.testName, func(t *testing.T) {
+			attr, err := s.GetAttributesByServerUUIDAndNamespace(tt.u, tt.ns)
+
+			if tt.expectedNotFound {
+				assert.Error(t, err)
+				assert.ErrorIs(t, err, db.ErrNotFound)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, attr)
+				assert.Equal(t, tt.expectedID, attr.ID)
+			}
+		})
+	}
+}
+
+func TestUpdateAttributesByServerUUIDAndNamespace(t *testing.T) {
+	s := db.DatabaseTest(t)
+
+	var testCases = []struct {
+		testName         string
+		u                uuid.UUID
+		ns               string
+		data             json.RawMessage
+		expectedNotFound bool
+	}{
+		{"happy path", db.FixtureServerDory.ID, db.FixtureNamespaceMetadata, json.RawMessage([]byte(`{"age":12,"location":"Fishbowl"}`)), false},
+		{"not found server uuid", uuid.New(), db.FixtureNamespaceMetadata, json.RawMessage{}, true},
+		{"not found namespace", db.FixtureServerDory.ID, "unknown", json.RawMessage{}, true},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.testName, func(t *testing.T) {
+			err := s.UpdateAttributesByServerUUIDAndNamespace(tt.u, tt.ns, tt.data)
+
+			if tt.expectedNotFound {
+				assert.Error(t, err)
+				assert.ErrorIs(t, err, db.ErrNotFound)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
 	}
 }
