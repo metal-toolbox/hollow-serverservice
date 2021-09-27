d9d7e044ed04bc250e3e61dc0248e0346e61db16
Ryan Hartje <ryan@ryanhartje.com>
2021-09-27 11:11:28 -0500

    remove deleted_at index

---
+++ db/migrations/00008_remove_deleted_at_index.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+-- +goose StatementBegin
+DROP INDEX idx_deleted_at;
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+CREATE INDEX idx_deleted_at ON servers (deleted_at) where deleted_at is null;
+-- +goose StatementEnd
