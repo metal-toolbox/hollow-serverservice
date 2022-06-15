941234357137de906201d8cec9d7182737b69782
Juan Antonio Osorio <juan.osoriorobles@eu.equinix.com>
2022-06-15 12:48:03 +0300

    Explicitly import postgres library
    This is now needed by the cockroach-go library.
    
    Signed-off-by: Juan Antonio Osorio <juan.osoriorobles@eu.equinix.com>

--- internal/dbtools/testtools.go
+++ internal/dbtools/testtools.go
@@ -11,6 +11,7 @@ import (
 	// import the crdbpgx for automatic retries of errors for crdb that support retry
 	_ "github.com/cockroachdb/cockroach-go/v2/crdb/crdbpgx"
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq" // Register the Postgres driver.
 	"github.com/stretchr/testify/require"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 
